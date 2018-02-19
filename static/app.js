f70da61e2fb15673f744ad0b10613b97bb535814
zc <z304307393@163.com>
2018-02-19 17:56:41 +0800

    修复

--- static/app.js
+++ static/app.js
@@ -7,6 +7,6 @@ const port = 3001
 // 文件静态化
 app.use('/', express.static(path.join(__dirname, 'dist'))) // 静态呈现页面
 
-const server = app.list(port, () => {
+const server = app.listen(port, () => {
     console.log('server init port:' + port)
 })
